da75d22d134cccc536260fae47e39d3cbfe62a81
IamSolaSholzy01 <65189840+IamSolaSholzy01@users.noreply.github.com>
2021-11-05 23:38:47 +0100

    Bug fixed

--- src/Components/SearchButton.js
+++ src/Components/SearchButton.js
@@ -2,7 +2,7 @@ import '../index.css'
 
 function SearchButton(){
   return(
-    <div class="search-buttons">
+    <div className="search-buttons">
       <button style={{marginRight: '1em'}}>Google Search</button>
       <button>I'm feeling lucky</button>
     </div>
